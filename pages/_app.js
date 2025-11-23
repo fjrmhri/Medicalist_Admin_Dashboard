d340b337956988555d7afd1b59ab6ca29a1006de
fjrmhri <132055200+fjrmhri@users.noreply.github.com>
2025-11-23 18:22:57 +0700

    Improve robustness and update Indonesian docs

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  // Bungkus seluruh halaman agar gaya global dan properti halaman tetap konsisten
   return <Component {...pageProps} />;
 }
 
